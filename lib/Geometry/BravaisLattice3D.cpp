b0369cece2766cdabc8e80ebf91cd943da5f0d52
Gyorgy J. Vida <vidagyorgy@gmail.com>
2017-01-10 23:33:56 +0100

    Fix BravaisLattice unique ptr to shared ptr

--- lib/Geometry/BravaisLattice3D.cpp
+++ lib/Geometry/BravaisLattice3D.cpp
@@ -11,6 +11,6 @@ BravaisLattice3D::BravaisLattice3D(const UnitCell3D& unit_cell_, const size_t x_
   , x_width(x_width_)
   , y_width(y_width_)
   , z_width(z_width_)
-  , lattice(LatticeGenerator(std::make_unique<CutoffUnitVectors>(unit_cell, x_width, y_width, z_width)).generate(true))
+  , lattice(LatticeGenerator(std::make_shared<CutoffUnitVectors>(unit_cell, x_width, y_width, z_width)).generate(true))
 {
 }
