a46acf775bc68a88743cad8ac20d8c5b9fa7eb84
Gyorgy J. Vida <vidagyorgy@gmail.com>
2017-02-28 04:34:22 +0100

    Refactor EnergyUpdate to separate file

---
+++ lib/Physics/CoreElectrons/EnergyUpdate.h
@@ -0,0 +1,78 @@
+#ifndef LATTICEGEN_ENERGYUPDATE_H
+#define LATTICEGEN_ENERGYUPDATE_H
+
+namespace Physics
+{
+  namespace CoreElectrons
+  {
+    struct EnergyUpdate
+    {
+      EnergyUpdate(unsigned int required_number_of_nodes_, double energy_tolerance_)
+        : required_number_of_nodes(required_number_of_nodes_), energy_tolerance(energy_tolerance_), lower(0.0),
+          upper(0.0), lower_set(false), upper_set(false) {}
+
+      double coarse(unsigned int number_of_nodes, double energy_)
+      {
+        double energy = energy_;
+        if (number_of_nodes > required_number_of_nodes) {
+          // energy is too large
+          // set or update upper energy bound
+          if (!upper_set) {
+            upper_set = true;
+            upper = energy;
+          }
+          if (energy < upper)
+            upper = energy;
+          if (lower_set)
+            // if we have a lower bound then bisect
+            energy = (upper + lower) / 2.0;
+          else
+            // else guess 10% less energy (energy is negative, less is more :) )
+            energy = 1.1 * upper;
+        } else {
+          // energy is too small
+          // set or update lower energy bound
+          if (!lower_set) {
+            lower_set = true;
+            lower = energy;
+          }
+          if (energy > lower)
+            lower = energy;
+          if (upper_set)
+            // if we have an upper bound then bisect
+            energy = (upper + lower) / 2.0;
+          else
+            // else guess 10% more energy
+            energy = 0.9 * lower;
+        }
+        return energy;
+      }
+
+      std::pair<double, bool> fine(double energy, double norm, double new_R, double new_dR_dr, double old_dR_dr_scaled)
+      {
+        double energy_diff = new_R * (new_dR_dr - old_dR_dr_scaled) / (2.0 * norm);
+
+        if (lower_set && (energy + energy_diff < lower))
+          return std::make_pair((energy + lower) / 2.0, false);
+        else if (energy + energy_diff > upper)
+          return std::make_pair((energy + upper) / 2.0, false);
+        else if (fabs(energy_diff / energy) > energy_tolerance)
+          return std::make_pair(energy + energy_diff, false);
+        else
+          return std::make_pair(energy, true);
+      }
+
+      const unsigned int required_number_of_nodes;
+      const double energy_tolerance;
+
+    private:
+      // TODO if C++17 is available rework this to optional
+      double lower;
+      double upper;
+      bool lower_set;
+      bool upper_set;
+    };
+  }
+}
+
+#endif //LATTICEGEN_ENERGYUPDATE_H
--- lib/Physics/CoreElectrons/RadialSchrodingerEquation.cpp
+++ lib/Physics/CoreElectrons/RadialSchrodingerEquation.cpp
@@ -1,76 +1,8 @@
 #include <Math/Integrator.h>
 #include "RadialSchrodingerEquation.h"
+#include "EnergyUpdate.h"
 
 using namespace Physics::CoreElectrons;
-
-namespace
-{
-  struct UpdateEnergy
-  {
-    UpdateEnergy(unsigned int required_number_of_nodes_, double energy_tolerance_)
-      : required_number_of_nodes(required_number_of_nodes_), energy_tolerance(energy_tolerance_), lower(0.0),
-        upper(0.0), lower_set(false), upper_set(false) {}
-
-    double coarse(unsigned int number_of_nodes, double energy_)
-    {
-      double energy = energy_;
-      if (number_of_nodes > required_number_of_nodes) {
-        // energy is too large
-        // set or update upper energy bound
-        if (!upper_set) {
-          upper_set = true;
-          upper = energy;
-        }
-        if (energy < upper)
-          upper = energy;
-        if (lower_set)
-          // if we have a lower bound then bisect
-          energy = (upper + lower) / 2.0;
-        else
-          // else guess 10% less energy (energy is negative, less is more :) )
-          energy = 1.1 * upper;
-      } else {
-        // energy is too small
-        // set or update lower energy bound
-        if (!lower_set) {
-          lower_set = true;
-          lower = energy;
-        }
-        if (energy > lower)
-          lower = energy;
-        if (upper_set)
-          // if we have an upper bound then bisect
-          energy = (upper + lower) / 2.0;
-        else
-          // else guess 10% more energy
-          energy = 0.9 * lower;
-      }
-      return energy;
-    }
-
-    std::pair<double, bool> fine(double energy, double norm, double new_R, double new_dR_dr, double old_dR_dr_scaled)
-    {
-      double energy_diff = new_R * (new_dR_dr - old_dR_dr_scaled) / (2.0 * norm);
-
-      if (lower_set && (energy + energy_diff < lower))
-        return std::make_pair((energy + lower) / 2.0, false);
-      else if (energy + energy_diff > upper)
-        return std::make_pair((energy + upper) / 2.0, false);
-      else if (fabs(energy_diff / energy) > energy_tolerance)
-        return std::make_pair(energy + energy_diff, false);
-      else
-        return std::make_pair(energy, true);
-    }
-
-    unsigned int required_number_of_nodes;
-    double energy_tolerance;
-    // TODO if C++17 is available rework this to optional
-    double lower;
-    double upper;
-    bool lower_set;
-    bool upper_set;
-  };
-}
 
 RadialSolution Physics::CoreElectrons::RadialSchrodingerEquation::solve(
   unsigned int n, unsigned int l, double energy) const
@@ -79,7 +11,7 @@ RadialSolution Physics::CoreElectrons::RadialSchrodingerEquation::solve(
   const auto dx = effective_charge.r->dx;
   const auto &z = effective_charge.z;
   const auto required_number_of_nodes = n - l - 1;
-  auto update_energy = UpdateEnergy(required_number_of_nodes, energy_tolerance);
+  auto update_energy = EnergyUpdate(required_number_of_nodes, energy_tolerance);
 
   std::vector<double> R(r.size(), 0.0);
   std::vector<double> dR_dr(r.size(), 0.0);
