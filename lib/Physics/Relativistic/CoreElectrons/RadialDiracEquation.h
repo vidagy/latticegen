ef139b4b6876635ee7fa7ea574d262fdbb28d237
Gyorgy J. Vida <vidagyorgy@gmail.com>
2017-05-14 05:15:44 +0200

    RadialDiracEquation: skeleton

---
+++ lib/Physics/Relativistic/CoreElectrons/RadialDiracEquation.h
@@ -0,0 +1,19 @@
+#ifndef LATTICEGEN_RADIALDIRACEQUATION_H
+#define LATTICEGEN_RADIALDIRACEQUATION_H
+
+namespace Physics
+{
+  namespace Relativistic
+  {
+    namespace CoreElectrons
+    {
+      class RadialDiracEquation
+      {
+
+      };
+    }
+  }
+}
+
+
+#endif //LATTICEGEN_RADIALDIRACEQUATION_H
