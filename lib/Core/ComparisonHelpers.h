43a52b82c048b4341f351333cfe2864006a890ce
Gyorgy J. Vida <vidagyorgy@gmail.com>
2017-05-31 04:18:55 +0200

    Housekeeping: fix naming

--- lib/Core/ComparisonHelpers.h
+++ lib/Core/ComparisonHelpers.h
@@ -5,12 +5,12 @@
 
 namespace Core
 {
-  static const double ABS_TOLERANCE = 1e-14;
-  static const double REL_TOLERANCE = 1e-10;
+  static const double default_abs_tolerance = 1e-14;
+  static const double default_rel_tolerance = 1e-10;
 
   inline bool equalsWithTolerance(
     double lhs, double rhs,
-    double abs_tolerance = ABS_TOLERANCE, double rel_tolerance = REL_TOLERANCE)
+    double abs_tolerance = default_abs_tolerance, double rel_tolerance = default_rel_tolerance)
   {
     double diff = fabs(lhs - rhs);
     if (diff > abs_tolerance)
@@ -21,47 +21,47 @@ namespace Core
       
   }
 
-  inline bool nearlyZero(double x, double abs_tolerance = ABS_TOLERANCE)
+  inline bool nearlyZero(double x, double abs_tolerance = default_abs_tolerance)
   {
     return x < abs_tolerance && x > -1.0 * abs_tolerance;
   }
 
-  inline bool strictlyGreater(double x, double y, double abs_tolerance = ABS_TOLERANCE)
+  inline bool strictlyGreater(double x, double y, double abs_tolerance = default_abs_tolerance)
   {
     return x > y + abs_tolerance;
   }
 
-  inline bool strictlyLess(double x, double y, double abs_tolerance = ABS_TOLERANCE)
+  inline bool strictlyLess(double x, double y, double abs_tolerance = default_abs_tolerance)
   {
     return strictlyGreater(y, x, abs_tolerance);
   }
 
-  inline bool greaterEqualsWithTolerance(double x, double y, double abs_tolerance = ABS_TOLERANCE)
+  inline bool greaterEqualsWithTolerance(double x, double y, double abs_tolerance = default_abs_tolerance)
   {
     return x > y - abs_tolerance;
   }
 
-  inline bool lessEqualsWithTolerance(double x, double y, double abs_tolerance = ABS_TOLERANCE)
+  inline bool lessEqualsWithTolerance(double x, double y, double abs_tolerance = default_abs_tolerance)
   {
     return greaterEqualsWithTolerance(y, x, abs_tolerance);
   }
 
-  inline bool strictlyPositive(double x, double abs_tolerance = ABS_TOLERANCE)
+  inline bool strictlyPositive(double x, double abs_tolerance = default_abs_tolerance)
   {
     return x > abs_tolerance;
   }
 
-  inline bool strictlyNegative(double x, double abs_tolerance = ABS_TOLERANCE)
+  inline bool strictlyNegative(double x, double abs_tolerance = default_abs_tolerance)
   {
     return x < -abs_tolerance;
   }
 
-  inline bool positiveWithTolerance(double x, double abs_tolerance = ABS_TOLERANCE)
+  inline bool positiveWithTolerance(double x, double abs_tolerance = default_abs_tolerance)
   {
     return ! strictlyNegative(x, abs_tolerance);
   }
 
-  inline bool negativeWithTolerance(double x, double abs_tolerance = ABS_TOLERANCE)
+  inline bool negativeWithTolerance(double x, double abs_tolerance = default_abs_tolerance)
   {
     return ! strictlyPositive(x, abs_tolerance);
   }
