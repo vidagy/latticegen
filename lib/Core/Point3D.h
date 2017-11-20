f679ea414ef3579bbcb47c9d95fe1a5b944413a9
Gyorgy J. Vida <vidagyorgy@gmail.com>
2017-11-20 21:39:42 +0100

    Eigen: replace Point3D to Eigen::Vector3d in Core

--- lib/Core/Point3D.h
+++ lib/Core/Point3D.h
@@ -7,139 +7,25 @@
 #include <sstream>
 #include <string>
 #include <cmath>
+#include "Mute.h"
+
+LATTICEGEN_MUTE_BEGIN
+LATTICEGEN_MUTE_EIGEN
+#include <Eigen/Dense>
+
+LATTICEGEN_MUTE_END
 
 #include <Core/ComparisonHelpers.h>
 
 namespace Core
 {
-  struct Point3D
-  {
-    Point3D(double x_ = 0.0, double y_ = 0.0, double z_ = 0.0)
-      : x(x_), y(y_), z(z_)
-    {}
+  typedef Eigen::Vector3d Point3D;
+  typedef Eigen::Ref<Eigen::Vector3d> Point3DRef;
+  typedef Eigen::Ref<const Eigen::Vector3d> Point3DCRef;
 
-    static Point3D create_polar(double r, double theta, double phi)
-    {
-      return Point3D{r * sin(theta) * cos(phi), r * sin(theta) * sin(phi), r * cos(theta)};
-    }
-    
-    // operators
-    Point3D &operator=(const Point3D &rhs) = default;
-
-    Point3D& operator+=(const Point3D& rhs)
-    {
-      this->x += rhs.x;
-      this->y += rhs.y;
-      this->z += rhs.z;
-      return *this;
-    }
-    Point3D& operator-=(const Point3D& rhs)
-    {
-      this->x -= rhs.x;
-      this->y -= rhs.y;
-      this->z -= rhs.z;
-      return *this;
-    }
-
-    Point3D &operator*=(double num)
-    {
-      this->x *= num;
-      this->y *= num;
-      this->z *= num;
-      return *this;
-    }
-
-    Point3D &operator/=(double num)
-    {
-      this->x /= num;
-      this->y /= num;
-      this->z /= num;
-      return *this;
-    }
-
-    double length() const
-    {
-      return sqrt( x*x + y*y + z*z );
-    }
-
-    double length2() const
-    {
-      return x * x + y * y + z * z;
-    }
-
-	  double x;
-    double y;
-    double z;
-  };
-
-  typedef Point3D Vector3D;
-
-  inline bool operator==(const Point3D& lhs, const Point3D& rhs)
-  {
-    return equalsWithTolerance(lhs.x, rhs.x) && 
-      equalsWithTolerance(lhs.y, rhs.y) && 
-      equalsWithTolerance(lhs.z, rhs.z);
-  }
-
-  inline Point3D operator+(const Point3D &lhs, const Point3D &rhs)
-  {
-    return Point3D{lhs.x + rhs.x, lhs.y + rhs.y, lhs.z + rhs.z};
-  }
-
-  inline Point3D operator-(const Point3D &lhs, const Point3D &rhs)
-  {
-    return Point3D{lhs.x - rhs.x, lhs.y - rhs.y, lhs.z - rhs.z};
-  }
-  inline Point3D operator-(Point3D lhs)
-  {
-    lhs.x = -lhs.x;
-    lhs.y = -lhs.y;
-    lhs.z = -lhs.z;
-    return lhs;
-  }
-
-  inline Point3D operator*(Point3D lhs, double num)
-  {
-    lhs *= num;
-    return lhs;
-  }
-
-  inline Point3D operator*(double num, Point3D rhs)
-  {
-    rhs *= num;
-    return rhs;
-  }
-
-  inline Point3D operator/(Point3D lhs, double num)
-  {
-    lhs /= num;
-    return lhs;
-  }
-  inline double operator*(Point3D lhs, const Point3D& rhs)
-  {
-    return lhs.x * rhs.x + lhs.y * rhs.y + lhs.z * rhs.z;
-  }
-  inline Point3D cross_product(Point3D lhs, const Point3D& rhs)
-  {
-    return Point3D(
-        lhs.y * rhs.z - lhs.z * rhs.y,
-        lhs.z * rhs.x - lhs.x * rhs.z,
-        lhs.x * rhs.y - lhs.y * rhs.x
-      );
-  }
-  inline void swap(Point3D& lhs, Point3D& rhs)
-  {
-    double tmp_x = lhs.x;
-    double tmp_y = lhs.y;
-    double tmp_z = lhs.z;
-
-    lhs.x = rhs.x;
-    lhs.y = rhs.y;
-    lhs.z = rhs.z;
-
-    rhs.x = tmp_x;
-    rhs.y = tmp_y;
-    rhs.z = tmp_z;
+  [[maybe_unused]]
+  static Point3D create_polar(double r, double theta, double phi) {
+    return Point3D{r * sin(theta) * cos(phi), r * sin(theta) * sin(phi), r * cos(theta)};
   }
 
   struct Point3DComparator
@@ -156,9 +42,9 @@ namespace Core
 
     bool isEqual(const Point3D &lhs, const Point3D &rhs) const
     {
-      return equalsWithTolerance(lhs.x, rhs.x, x_abs_tol, x_rel_tol) &&
-             equalsWithTolerance(lhs.y, rhs.y, y_abs_tol, y_rel_tol) &&
-             equalsWithTolerance(lhs.z, rhs.z, z_abs_tol, z_rel_tol);
+      return equalsWithTolerance(lhs(0), rhs(0), x_abs_tol, x_rel_tol) &&
+             equalsWithTolerance(lhs(1), rhs(1), y_abs_tol, y_rel_tol) &&
+             equalsWithTolerance(lhs(2), rhs(2), z_abs_tol, z_rel_tol);
     }
 
     const double x_abs_tol;
@@ -168,24 +54,20 @@ namespace Core
     const double y_rel_tol;
     const double z_rel_tol;
   };
+
+  static const auto default_point_comparator =
+    Point3DComparator(Core::default_abs_tolerance, Core::default_rel_tolerance);
 }
 
 namespace std
 {
   using namespace Core;
-  inline std::string to_string(const Point3D& point)
+
+  inline std::string to_string(const Point3DCRef &point)
   {
     std::stringstream ss;
-    ss << std::scientific << std::setprecision(std::numeric_limits< double >::max_digits10)
-       << "(" << std::setw(24) << point.x << " , " << std::setw(24) << point.y << " , " << std::setw(24) << point.z
-       << " )";
+    ss << std::scientific << std::setprecision(std::numeric_limits<double>::max_digits10) << point;
     return ss.str();
-  }
-
-  inline ostream &operator<<(ostream &o, const Core::Point3D &p)
-  {
-    o << to_string(p);
-    return o;
   }
 }
 #endif // LATTICEGEN_POINT3D_H_
--- test/Core/Test_Point3D.cpp
+++ test/Core/Test_Point3D.cpp
@@ -7,54 +7,41 @@ using namespace Core;
 
 TEST(TestPoint3D,Ctor)
 {
-  const Point3D p = Point3D(1.0, 2.0, 3.0);
-  EXPECT_DOUBLE_EQ(1.0, p.x );
-  EXPECT_DOUBLE_EQ(2.0, p.y );
-  EXPECT_DOUBLE_EQ(3.0, p.z );
+  const auto p = Point3D(1.0, 2.0, 3.0);
+  EXPECT_DOUBLE_EQ(1.0, p(0));
+  EXPECT_DOUBLE_EQ(2.0, p(1));
+  EXPECT_DOUBLE_EQ(3.0, p(2));
 }
 
 TEST(TestPoint3D, CreatPolar)
 {
-  auto xp = Point3D::create_polar(1.0, pi / 2.0, 0.0);
+  auto xp = create_polar(1.0, pi / 2.0, 0.0);
   auto x = Point3D(1.0, 0.0, 0.0);
-  EXPECT_EQ(xp, x);
+  EXPECT_TRUE(default_point_comparator.isEqual(xp, x));
 
-  auto yp = Point3D::create_polar(1.0, pi / 2.0, pi / 2.0);
+  auto yp = create_polar(1.0, pi / 2.0, pi / 2.0);
   auto y = Point3D(0.0, 1.0, 0.0);
-  EXPECT_EQ(yp, y);
+  EXPECT_TRUE(default_point_comparator.isEqual(yp, y));
 
-  auto zp = Point3D::create_polar(1.0, 0.0, 0.0);
+  auto zp = create_polar(1.0, 0.0, 0.0);
   auto z = Point3D(0.0, 0.0, 1.0);
-  EXPECT_EQ(zp, z);
-}
-
-TEST(TestPoint3D,Swap)
-{
-  Point3D p = Point3D(1.0,2.0,3.0);
-  Point3D q = Point3D(10.0,20.0,30.0);
-  swap(p,q);
-  EXPECT_DOUBLE_EQ(10.0, p.x );
-  EXPECT_DOUBLE_EQ(20.0, p.y );
-  EXPECT_DOUBLE_EQ(30.0, p.z );
-  EXPECT_DOUBLE_EQ(1.0, q.x );
-  EXPECT_DOUBLE_EQ(2.0, q.y );
-  EXPECT_DOUBLE_EQ(3.0, q.z );
+  EXPECT_TRUE(default_point_comparator.isEqual(zp, z));
 }
 
 TEST(TestPoint3D,Copy)
 {
-  const Point3D p = Point3D(1.0,2.0,3.0);
-  const Point3D q = p;
+  const auto p = Point3D(1.0, 2.0, 3.0);
+  const auto q = p;
   EXPECT_TRUE( &q != &p);
-  EXPECT_DOUBLE_EQ(p.x, q.x );
-  EXPECT_DOUBLE_EQ(p.y, q.y );
-  EXPECT_DOUBLE_EQ(p.z, q.z );
+  EXPECT_DOUBLE_EQ(p(0), q(0));
+  EXPECT_DOUBLE_EQ(p(1), q(1));
+  EXPECT_DOUBLE_EQ(p(2), q(2));
 
   const Point3D r(p);
   EXPECT_TRUE( &r != &p);
-  EXPECT_DOUBLE_EQ(p.x, r.x );
-  EXPECT_DOUBLE_EQ(p.y, r.y );
-  EXPECT_DOUBLE_EQ(p.z, r.z );
+  EXPECT_DOUBLE_EQ(p(0), r(0));
+  EXPECT_DOUBLE_EQ(p(1), r(1));
+  EXPECT_DOUBLE_EQ(p(2), r(2));
 }
 
 TEST(TestPoint3D,Plus)
@@ -63,110 +50,110 @@ TEST(TestPoint3D,Plus)
   const Point3D q = Point3D(10.0,20.0,30.0);
   
   const Point3D pq = p + q;
-  EXPECT_DOUBLE_EQ(pq.x, p.x + q.x );
-  EXPECT_DOUBLE_EQ(pq.y, p.y + q.y );
-  EXPECT_DOUBLE_EQ(pq.z, p.z + q.z );
+  EXPECT_DOUBLE_EQ(pq(0), p(0) + q(0));
+  EXPECT_DOUBLE_EQ(pq(1), p(1) + q(1));
+  EXPECT_DOUBLE_EQ(pq(2), p(2) + q(2));
   
   const Point3D qp = q + p;
-  EXPECT_DOUBLE_EQ(qp.x, p.x + q.x );
-  EXPECT_DOUBLE_EQ(qp.y, p.y + q.y );
-  EXPECT_DOUBLE_EQ(qp.z, p.z + q.z );
+  EXPECT_DOUBLE_EQ(qp(0), p(0) + q(0));
+  EXPECT_DOUBLE_EQ(qp(1), p(1) + q(1));
+  EXPECT_DOUBLE_EQ(qp(2), p(2) + q(2));
 
   Point3D r = Point3D(100.0,200.0,300.0);
   r += p;
-  EXPECT_DOUBLE_EQ(r.x, p.x + 100.0 );
-  EXPECT_DOUBLE_EQ(r.y, p.y + 200.0 );
-  EXPECT_DOUBLE_EQ(r.z, p.z + 300.0 );
+  EXPECT_DOUBLE_EQ(r(0), p(0) + 100.0);
+  EXPECT_DOUBLE_EQ(r(1), p(1) + 200.0);
+  EXPECT_DOUBLE_EQ(r(2), p(2) + 300.0);
   r += q;
-  EXPECT_DOUBLE_EQ(r.x, p.x + q.x + 100.0 );
-  EXPECT_DOUBLE_EQ(r.y, p.y + q.y + 200.0 );
-  EXPECT_DOUBLE_EQ(r.z, p.z + q.z + 300.0 );
+  EXPECT_DOUBLE_EQ(r(0), p(0) + q(0) + 100.0);
+  EXPECT_DOUBLE_EQ(r(1), p(1) + q(1) + 200.0);
+  EXPECT_DOUBLE_EQ(r(2), p(2) + q(2) + 300.0);
 }
 
 TEST(TestPoint3D,Minus)
 {
-  const Point3D p = Point3D(1.0,2.0,3.0);
-  const Point3D q = Point3D(10.0,20.0,30.0);
-  
-  const Point3D pq = p - q;
-  EXPECT_DOUBLE_EQ(pq.x, p.x - q.x );
-  EXPECT_DOUBLE_EQ(pq.y, p.y - q.y );
-  EXPECT_DOUBLE_EQ(pq.z, p.z - q.z );
-  
-  const Point3D qp = q - p;
-  EXPECT_DOUBLE_EQ(qp.x, q.x - p.x );
-  EXPECT_DOUBLE_EQ(qp.y, q.y - p.y );
-  EXPECT_DOUBLE_EQ(qp.z, q.z - p.z );
+  const auto p = Point3D(1.0, 2.0, 3.0);
+  const auto q = Point3D(10.0, 20.0, 30.0);
+
+  const auto pq = p - q;
+  EXPECT_DOUBLE_EQ(pq(0), p(0) - q(0));
+  EXPECT_DOUBLE_EQ(pq(1), p(1) - q(1));
+  EXPECT_DOUBLE_EQ(pq(2), p(2) - q(2));
+
+  const auto qp = q - p;
+  EXPECT_DOUBLE_EQ(qp(0), q(0) - p(0));
+  EXPECT_DOUBLE_EQ(qp(1), q(1) - p(1));
+  EXPECT_DOUBLE_EQ(qp(2), q(2) - p(2));
 
   Point3D r = Point3D(100.0,200.0,300.0);
   r -= p;
-  EXPECT_DOUBLE_EQ(r.x, 100.0 - p.x );
-  EXPECT_DOUBLE_EQ(r.y, 200.0 - p.y );
-  EXPECT_DOUBLE_EQ(r.z, 300.0 - p.z );
+  EXPECT_DOUBLE_EQ(r(0), 100.0 - p(0));
+  EXPECT_DOUBLE_EQ(r(1), 200.0 - p(1));
+  EXPECT_DOUBLE_EQ(r(2), 300.0 - p(2));
   r -= q;
-  EXPECT_DOUBLE_EQ(r.x, 100.0 - p.x - q.x);
-  EXPECT_DOUBLE_EQ(r.y, 200.0 - p.y - q.y);
-  EXPECT_DOUBLE_EQ(r.z, 300.0 - p.z - q.z);
+  EXPECT_DOUBLE_EQ(r(0), 100.0 - p(0) - q(0));
+  EXPECT_DOUBLE_EQ(r(1), 200.0 - p(1) - q(1));
+  EXPECT_DOUBLE_EQ(r(2), 300.0 - p(2) - q(2));
 }
 
 TEST(TestPoint3D,MultiplicationWithDouble)
 {
-  const Point3D p = Point3D(1.0,2.0,3.0);
+  const auto p = Point3D(1.0, 2.0, 3.0);
 
-  const Point3D q = p * 2.0;
-  EXPECT_DOUBLE_EQ(2.0, q.x );
-  EXPECT_DOUBLE_EQ(4.0, q.y );
-  EXPECT_DOUBLE_EQ(6.0, q.z );
+  const auto q = p * 2.0;
+  EXPECT_DOUBLE_EQ(2.0, q(0));
+  EXPECT_DOUBLE_EQ(4.0, q(1));
+  EXPECT_DOUBLE_EQ(6.0, q(2));
 
-  const Point3D r = 2.0 * p;
-  EXPECT_DOUBLE_EQ(2.0, r.x );
-  EXPECT_DOUBLE_EQ(4.0, r.y );
-  EXPECT_DOUBLE_EQ(6.0, r.z );
+  const auto r = 2.0 * p;
+  EXPECT_DOUBLE_EQ(2.0, r(0));
+  EXPECT_DOUBLE_EQ(4.0, r(1));
+  EXPECT_DOUBLE_EQ(6.0, r(2));
 
-  Point3D s = Point3D(1.0,2.0,3.0);
+  auto s = Point3D(1.0, 2.0, 3.0);
   s *= 3.0;
-  EXPECT_DOUBLE_EQ(3.0, s.x );
-  EXPECT_DOUBLE_EQ(6.0, s.y );
-  EXPECT_DOUBLE_EQ(9.0, s.z );
+  EXPECT_DOUBLE_EQ(3.0, s(0));
+  EXPECT_DOUBLE_EQ(6.0, s(1));
+  EXPECT_DOUBLE_EQ(9.0, s(2));
 }
 
 TEST(TestPoint3D,ScalarProduct)
 {
-  const Point3D p = Point3D(1.0,2.0,3.0);
-  const Point3D q = Point3D(10.0,20.0,30.0);
+  const auto p = Point3D(1.0, 2.0, 3.0);
+  const auto q = Point3D(10.0, 20.0, 30.0);
 
-  const double pq = p * q;
+  const double pq = p.dot(q);
   EXPECT_DOUBLE_EQ(140.0, pq );
-  
-  const double qp = q * p;
+
+  const double qp = q.dot(p);
   EXPECT_DOUBLE_EQ(140.0, qp );
 
 }
 
 TEST(TestPoint3D,CrossProduct)
 {
-  const Point3D p = Point3D(1.0,0.0,0.0);
-  const Point3D q = Point3D(0.0,1.0,0.0);
+  const auto p = Point3D(1.0, 0.0, 0.0);
+  const auto q = Point3D(0.0, 1.0, 0.0);
 
-  const Point3D pxq = cross_product(p, q);
+  const auto pxq = p.cross(q);
   EXPECT_EQ(Point3D(0.0, 0.0, 1.0), pxq );
-  
-  const Point3D qxp = cross_product(q, p);
+
+  const auto qxp = q.cross(p);
   EXPECT_EQ(Point3D(0.0, 0.0, -1.0), qxp );
 
 }
 
 TEST(TestPoint3D,Compare)
 {
-  const Point3D p = Point3D( 1.0, 2.0, 3.0);
-  const Point3D q = Point3D( 1.0, 2.0, 3.0);
-  const Point3D r = Point3D(10.0, 2.0, 3.0);
-  const Point3D s = Point3D( 1.0,20.0, 3.0);
-  const Point3D t = Point3D( 1.0, 2.0,30.0);
-  const Point3D u = Point3D(10.0,20.0, 3.0);
-  const Point3D v = Point3D( 1.0,20.0,30.0);
-  const Point3D w = Point3D(10.0, 2.0,30.0);
-  const Point3D x = Point3D(10.0, 2.0,30.0);
+  const auto p = Point3D(1.0, 2.0, 3.0);
+  const auto q = Point3D(1.0, 2.0, 3.0);
+  const auto r = Point3D(10.0, 2.0, 3.0);
+  const auto s = Point3D(1.0, 20.0, 3.0);
+  const auto t = Point3D(1.0, 2.0, 30.0);
+  const auto u = Point3D(10.0, 20.0, 3.0);
+  const auto v = Point3D(1.0, 20.0, 30.0);
+  const auto w = Point3D(10.0, 2.0, 30.0);
+  const auto x = Point3D(10.0, 2.0, 30.0);
 
   EXPECT_TRUE(p == q);
   EXPECT_FALSE(p == r);
@@ -180,16 +167,16 @@ TEST(TestPoint3D,Compare)
 
 TEST(TestPoint3D, CompareWithTolerance)
 {
-  const Point3D p = Point3D(1.0, 2.0, 3.0);
-  const Point3D q = Point3D(1.01, 2.0, 3.0);
+  const auto p = Point3D(1.0, 2.0, 3.0);
+  const auto q = Point3D(1.01, 2.0, 3.0);
 
   auto comparator1 = Point3DComparator(0.1, 1.0);
   EXPECT_TRUE(comparator1.isEqual(p, q));
   auto comparator2 = Point3DComparator(0.001, 1.0);
   EXPECT_FALSE(comparator2.isEqual(p, q));
 
-  const Point3D r = Point3D(100.0, 200.0, 300.0);
-  const Point3D s = Point3D(100.01, 200.0, 300.0);
+  const auto r = Point3D(100.0, 200.0, 300.0);
+  const auto s = Point3D(100.01, 200.0, 300.0);
 
   auto comparator3 = Point3DComparator(0.1, 1e-3);
   EXPECT_TRUE(comparator3.isEqual(r, s));
